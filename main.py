0c9bec798f351203e3ff3782611d33151818a2c4
mkubincova <mkubincova@proton.me>
2024-01-25 13:38:12 +0100

    feat: text, highligths

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from tkinter import *
+from text import sentences
+import random
 
+random.shuffle(sentences)
+text = " ".join(sentences)
 timer = None
-text = "hello there"
 position = 0
 chars = 0
 correct_chars = 0
@@ -9,8 +12,10 @@ correct_chars = 0
 
 def start_timer():
     start_btn.pack_forget()
-    text_widget.pack()
+    timer_label.pack()
+    text_widget.pack(side='top',fill='both',expand=True)
     user_input.pack()
+    user_input.focus()
     countdown(60)
 
 
@@ -23,29 +28,45 @@ def countdown(count):
         end_test()
 
 
+def update_current_char(pos):
+    text_widget.tag_remove('current', f"1.0", "end")
+    text_widget.tag_add('current', f"1.{pos}", f"1.{pos + 1}")
+    text_widget.tag_config('current', underline=True, underlinefg='yellow')
+
+
+def set_incorrect_char(pos):
+    text_widget.tag_add("incorrect", f"1.{pos}", f"1.{pos + 1}")
+    text_widget.tag_config("incorrect", background="red")
+
+
+def set_corrected_char(pos):
+    text_widget.tag_remove("incorrect", f"1.{pos}", f"1.{pos + 1}")
+    text_widget.tag_add("correct", f"1.{pos}", f"1.{pos + 1}")
+    text_widget.tag_config("correct", background="blue")
+
+
 def on_user_input(event):
     global position
     global chars
-    global timer
     global correct_chars
+    global text
 
     if event.keysym == 'space':
         user_input.delete(0, END)
-        position = position + 1
-    else:
+    if event.char:
         chars = chars + 1
         letter = event.char
         if letter == text[position]:
             correct_chars = correct_chars + 1
-            if text_widget.tag_names(f"1.{position}"):
-                text_widget.tag_add("correct", f"1.{position}", f"1.{position + 1}")
-                text_widget.tag_config("correct", background="green")
+            if "incorrect" in text_widget.tag_names(f"1.{position}"):
+                set_corrected_char(position)
             position = position + 1
         else:
-            text_widget.tag_add("incorrect", f"1.{position}", f"1.{position + 1}")
-            text_widget.tag_config("incorrect", background="red")
+            set_incorrect_char(position)
 
-    if position > len(text) - 1:
+    update_current_char(position)
+
+    if position == len(text) - 1:
         end_test()
 
 
@@ -54,30 +75,46 @@ def end_test():
     window.after_cancel(timer)
     user_input.pack_forget()
     chars_minute = correct_chars / (60 - int(timer_label["text"])) * 60
-    accuracy = correct_chars / chars * 100
-    speed_label.config(text=f"chars per minute: {'{:.2f}'.format(chars_minute)}")
-    accuracy_label.config(text=f"your accuracy is: {'{:.2f}%'.format(accuracy)}")
+    accuracy = correct_chars / chars * 100 if chars else 0
+    speed_label.config(text=f"Characters per minute: {'{:.2f}'.format(chars_minute)}")
+    accuracy_label.config(text=f"Accuracy: {'{:.2f}%'.format(accuracy)}")
     speed_label.pack()
     accuracy_label.pack()
+
+
+def center_window():
+    window_height = 800
+    window_width = 800
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x_cor = int((screen_width / 2) - (window_width / 2))
+    y_cor = int((screen_height / 2) - (window_height / 2))
+    window.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cor, y_cor))
+
 
 # UI
 window = Tk()
 window.title("Type speed test")
 window.config(padx=50, pady=50)
+center_window()
 
-timer_label = Label(text="60")
-timer_label.pack()
+heading_label = Label(text="Typing speed test", font=("Arial", 40), pady=20)
+heading_label.pack()
 
-text_widget = Text()
-text_widget.insert(INSERT, text)
+timer_label = Label(text="60", font=("Arial", 25), pady=10)
+
+text_widget = Text(font=("Arial", 20), width=1, height=1, spacing2=10, wrap=WORD)
+text_widget.insert(END, text)
+text_widget.tag_add('current', f"1.{position}", f"1.{position + 1}")
+text_widget.tag_config('current', underline=True, underlinefg='yellow')
 
 user_input = Entry()
-user_input.bind('<KeyRelease>', on_user_input)
+user_input.bind('<KeyPress>', on_user_input)
 
-start_btn = Button(text="Start", command=start_timer)
+start_btn = Button(text="Start", command=start_timer, font=("Arial", 20), padx=10, pady=7)
 start_btn.pack()
 
-speed_label = Label(text="")
-accuracy_label = Label(text="")
+speed_label = Label(text="", font=("Arial", 20))
+accuracy_label = Label(text="", font=("Arial", 20))
 
 window.mainloop()
---
+++ text.py
@@ -0,0 +1,12 @@
+sentences = [
+    "The pigs were insulted that they were named hamburgers.",
+    "Just go ahead and press that button.",
+    "You bite up because of your lower jaw.",
+    "She folded her handkerchief neatly.",
+    "She finally understood that grief was her love with no place for it to go.",
+    "The external scars tell only part of the story.",
+    "I often see the time 11:11 or 12:34 on clocks.",
+    "I'm not a party animal, but I do like animal parties.",
+    "Dan ate the clouds like cotton candy.",
+    "She lived on Monkey Jungle Road and that seemed to explain all of her strangeness."
+]
